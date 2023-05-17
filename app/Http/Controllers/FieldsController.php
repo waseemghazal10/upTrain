002cdba64b07709baef3b90084f1da099193283d
OlaAbdallah1 <olaabdallah2014@gmail.com>
2023-05-18 02:41:03 +0300

    join fields with employees

--- app/Http/Controllers/FieldsController.php
+++ app/Http/Controllers/FieldsController.php
@@ -13,9 +13,21 @@ class FieldsController extends Controller
 
     function getFields()
     {
-        $fields = Field::where('fields.id', '!=', '17')->join('colleges','colleges.id','fields.college_id')->get();
+        $fields = Field::where('fields.id', '!=', '17')->join('colleges', 'colleges.id', 'fields.college_id')
+            ->get();
 
-        $response =  $fields;
+        // $fieldsWEmployee = $fields->join('employees', 'employees.field_id', 'fields.id')->get();
+        $fieldsWEmployee = Field::join('employees', 'employees.field_id', 'fields.id')
+            ->where('field_id', '!=', '17')->where('employees.eRole','!=','1')->join('users', 'users.id', 'employees.user_id')
+            ->get();
+
+
+        if ($fieldsWEmployee->count() != 0) {
+            $response =  $fieldsWEmployee;
+            
+        } else {
+            $response =  $fields;
+        }
 
         return response($response, 201);
     }

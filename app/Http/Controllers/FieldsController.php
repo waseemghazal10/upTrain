79e976891d0e5610066386220e5f12fb2a1e3bbe
OlaAbdallah1 <olaabdallah2014@gmail.com>
2023-05-18 03:03:04 +0300

    join fields with employees need to discussed!!! + deleteField

--- app/Http/Controllers/FieldsController.php
+++ app/Http/Controllers/FieldsController.php
@@ -13,33 +13,39 @@ class FieldsController extends Controller
 
     function getFields()
     {
+        $fields = Field::join('colleges', 'colleges.id', 'fields.college_id')->get();
+        $response =  $fields;
+
+        return response($response, 201);
+    }
+
+    function deleteField($id)
+    {
+        $field = Field::find($id);
+        // where('fName', $name)->get();
+        $field->delete();
+        $response =  'Deleted field ' . $field->fName;
+
+        return response($response, 201);
+    }
+
+    function getEmployeesFields(Request $request)
+    {
         $fields = Field::where('fields.id', '!=', '17')->join('colleges', 'colleges.id', 'fields.college_id')
             ->get();
 
         // $fieldsWEmployee = $fields->join('employees', 'employees.field_id', 'fields.id')->get();
-        $fieldsWEmployee = Field::join('employees', 'employees.field_id', 'fields.id')
-            ->where('field_id', '!=', '17')->where('employees.eRole','!=','1')->join('users', 'users.id', 'employees.user_id')
+        $fieldsWEmployee = Field::join('employees', 'employees.field_id', 'fields.id')->join('colleges', 'colleges.id', 'fields.college_id')
+            ->where('employees.eRole', '!=', '1')->join('users', 'users.id', 'employees.user_id')
             ->get();
 
 
         if ($fieldsWEmployee->count() != 0) {
             $response =  $fieldsWEmployee;
-            
         } else {
             $response =  $fields;
         }
 
         return response($response, 201);
     }
-
-    // function getEmployeesFields(Request $request)
-    // {
-    //     $fields = Field::join('employees', 'employees.field_id', 'fields.id')
-    //         ->where('field_id', '!=', 17)->join('users', 'users.id', 'employees.user_id')
-    //         ->get();
-
-    //     $response =  $fields;
-
-    //     return response($response, 201);
-    // }
 }

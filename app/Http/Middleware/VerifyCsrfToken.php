dc929609b8bc85edc8a090bf0180f9df04dabc77
waseemghazal10 <waseemghazal10@gmail.com>
2023-06-03 18:54:45 +0300

    change password

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,7 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         // 'http://127.0.0.1:8000/api/register?email=waseemghazal@gmail.com&firstName=waseem&lastName=ghazal&phone=0597567681&picture=alknfkslf&password=Qqqq1234',
-
+        // '/api/register',
         '/requestResetPassword',
         '/verifyResetPassword'
     ];
